ac2df303b8b3c57f174dc7f24d2f4370aa660e7e
Casey Tansey <ctansey12600@outlook.com>
2022-09-07 20:10:21 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,12 @@ class String
   end
 
   def count_sentences
+    # Takes self
+    # splits self each time there is a ".","?", "!" present
+    # Takes split sentence values in array filters each value (sentence)
+    # Test if sentence is empty -> if sentence not empty -> remains in array
+    # If sentence is emptry -> deleted from array
+    # Then measures size of final array
     self.split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.size
   end
 end
